53ef4184846686290dc9e1b191a2d4fdd7721d5d
Dreamerrrr <27842508+Dreamerrrr@users.noreply.github.com>
2020-04-07 21:25:30 -0600

    Update build_template.go
    add_magisk() function added

--- templates/build_template.go
+++ templates/build_template.go
@@ -384,6 +384,7 @@ full_run() {
   fi
   add_chromium
   build_aosp
+  add_magisk
   release "${DEVICE}"
   aws_upload
   checkpoint_versions
@@ -1131,6 +1132,78 @@ build_aosp() {
 get_radio_image() {
   grep -Po "require version-$1=\K.+" vendor/$2/vendor-board-info.txt | tr '[:upper:]' '[:lower:]'
 }
+
+add_magisk(){
+  log_header ${FUNCNAME}
+
+  if [ -z ${BUILD_NUMBER+x} ]; then
+    BUILD_NUMBER=$(cat $BUILD_DIR/out/build_number.txt 2>/dev/null)
+  fi
+
+  mkdir -p $HOME/magisk
+
+  cd $HOME/magisk
+
+  # Download latest Magisk release
+  curl -s https://api.github.com/repos/topjohnwu/Magisk/releases | grep "Magisk-v.*.zip" |grep https|head -n 1| cut -d : -f 2,3|tr -d \" | wget -O magisk-latest.zip -qi -
+  
+  # Extract the downloaded zip
+  unzip -d magisk-latest magisk-latest.zip 
+
+  # Move the original init binary to the place where Magisk expects it to be
+  mkdir -p $BUILD_DIR/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/aosp_$DEVICE-target_files-$BUILD_NUMBER/BOOT/RAMDISK/.backup
+  cp -an $BUILD_DIR/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/aosp_$DEVICE-target_files-$BUILD_NUMBER/BOOT/RAMDISK/{init,.backup/init}
+  rm -f $BUILD_DIR/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/aosp_$DEVICE-target_files-$BUILD_NUMBER/BOOT/RAMDISK/init
+
+  # Copy the downloaded magiskinit binary to the place of the original init binary
+  cp magisk-latest/arm/magiskinit64 $BUILD_DIR/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/aosp_$DEVICE-target_files-$BUILD_NUMBER/BOOT/RAMDISK/init
+
+  # Create Magisk config file. We keep dm-verity and encryptiong.
+  cat <<EOF > $BUILD_DIR/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/aosp_$DEVICE-target_files-$BUILD_NUMBER/BOOT/RAMDISK/.backup/.magisk
+KEEPFORCEENCRYPT=true
+KEEPVERITY=true
+RECOVERYMODE=false
+EOF
+
+  # Add our "new" files to the list of files to be packaged/compressed/embedded into the final BOOT image
+  sed -i "/firmware 0 0 644/a .backup 0 0 000 selabel=u:object_r:rootfs:s0 capabilities=0x0\n.backup/init 0 2000 750 selabel=u:object_r:init_exec:s0 capabilities=0x0\n.backup/.magisk 0 2000 750 selabel=u:object_r:rootfs:s0 capabilities=0x0" $BUILD_DIR/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/aosp_$DEVICE-target_files*/META/boot_filesystem_config.txt
+
+  # Retrieve extract-dtb script that will allow us to separate already compiled binary and the concatenated DTB files
+  git clone https://github.com/PabloCastellano/extract-dtb.git
+
+  # Separate kernel and separate DTB files
+  cd extract-dtb
+  python3 ./extract-dtb.py $BUILD_DIR/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/aosp_$DEVICE-target_files-$BUILD_NUMBER/BOOT/kernel
+
+  # Uncompress the kernel
+  lz4 -d dtb/00_kernel dtb/uncompressed_kernel
+  cd -
+
+  # Hexpatch the kernel
+  chmod +x ./magisk-latest/x86/magiskboot
+  ./magisk-latest/x86/magiskboot hexpatch extract-dtb/dtb/uncompressed_kernel 736B69705F696E697472616D667300 77616E745F696E697472616D667300
+
+  # Recompress kernel
+  lz4 -f -9 extract-dtb/dtb/uncompressed_kernel extract-dtb/dtb/00_kernel
+  rm extract-dtb/dtb/uncompressed_kernel
+
+  # Concatenate back kernel and DTB files
+  rm $BUILD_DIR/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/aosp_$DEVICE-target_files-$BUILD_NUMBER/BOOT/kernel
+  for file in extract-dtb/dtb/*
+  do
+    cat $file >> $BUILD_DIR/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/aosp_$DEVICE-target_files-$BUILD_NUMBER/BOOT/kernel
+  done
+
+  # Remove target files zip
+  rm -f $BUILD_DIR/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/aosp_$DEVICE-target_files-$BUILD_NUMBER.zip
+
+  # Rezip target files
+  cd $BUILD_DIR/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/aosp_$DEVICE-target_files-$BUILD_NUMBER
+  zip --symlinks -r ../aosp_$DEVICE-target_files-$BUILD_NUMBER.zip *
+  cd -
+
+}
+
 
 release() {
   log_header ${FUNCNAME}
